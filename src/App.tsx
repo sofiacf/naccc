9ab5f3b86ef668a46d64fb36d89fd93fd3fad64e
Sofia <dev@smcf.io>
2019-10-13 20:23:27 -0400

    Setup ssl

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 export const App: React.FC = () => <>
     <Router>
-        <div className="App">
-            <Switch>
-                <Route path='/.well-known/acme-challenge/QKvmMg1sYEAtf3Z6XHN4ogKnfplHDl67oKgtEzj7IJ0'>
-                    QKvmMg1sYEAtf3Z6XHN4ogKnfplHDl67oKgtEzj7IJ0.BxLnj-VcmdVXujyF0rBFpoJrfDtqnp3FwLjU5YOdag0
-                </Route>
-                <Route path='/'>
+        <Switch>
+            <Route path='/.well-known/acme-challenge/QKvmMg1sYEAtf3Z6XHN4ogKnfplHDl67oKgtEzj7IJ0'>
+                QKvmMg1sYEAtf3Z6XHN4ogKnfplHDl67oKgtEzj7IJ0.BxLnj-VcmdVXujyF0rBFpoJrfDtqnp3FwLjU5YOdag0
+            </Route>
+            <Route path='/'>
+                <div className="App">
                     <header className="App-header">
                         <p>Welcome to NACCC 2020 Boston</p>
                         <p>Please email contact@naccc2020.com with any inquiries</p>
                         <p>We can't wait to see you Labor Day weekend 2020!</p>
                     </header>
-                </Route>
-            </Switch>
-        </div>
+                </div>
+            </Route>
+        </Switch>
     </Router>
 </>
