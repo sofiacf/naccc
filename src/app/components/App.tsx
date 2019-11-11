f230ed2bfdd53eadf84b5d869350854aa1d2bf23
Sofia <dev@smcf.io>
2019-11-11 17:38:58 -0500

    Change nav titles
    Remove about; rename sponsorships & merch
    
    [Resolves #7]

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import img from '../images/naccc.png'
 import { Nav } from './Nav'
 import { Home } from './Home'
-import { About } from './About'
 import { Sponsors } from './Sponsors'
 import { Contact } from './Contact'
 
@@ -11,7 +10,6 @@ export const App: React.FC = () => <Router>
     <a className="logo" href="/"><img src={ img } alt='NACCC 2020'/></a>
     <Nav/>
     <Route path="/" component={ Home } exact/>
-    <Route path="/about" component={ About }/>
-    <Route path="/sponsors" component={ Sponsors }/>
+    <Route path="/sponsorships" component={ Sponsors }/>
     <Route path="/contact" component={ Contact }/>
 </Router>
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-function getNavLink(page: string): JSX.Element {
-    return <li><NavLink key={ page } to={ `/${ page.toLowerCase() }` }>{ page }</NavLink></li>
-}
-
-const shopLink = <li><a key="Shop" href='https://bbma.bigcartel.com/'>Shop</a></li>
-const links = [getNavLink('About'), getNavLink('Sponsors'), shopLink, getNavLink('Contact')]
-
 export const Nav: React.FC = () => <nav title="navigation">
-    <ul> { links } </ul>
+    <ul>
+        <li><NavLink to='/sponsorships'>SPONSORSHIP</NavLink></li>
+        <li><NavLink to='/contact'>CONTACT</NavLink></li>
+        <li><a key="Shop" href='https://bbma.bigcartel.com/'>MERCH</a></li>
+    </ul>
 </nav>
