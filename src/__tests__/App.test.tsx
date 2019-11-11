8c925794b18470ccb937c338df5ef1fbc40ffdd9
Sofia <dev@smcf.io>
2019-11-11 17:36:45 -0500

    Enhance test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
 import { App } from '../app/components/App'
 
+const el = document.createElement('div')
+
+const renderApp = () => render(<App/>, el)
+const unmount = () => unmountComponentAtNode(el)
+
+beforeEach(renderApp)
+afterEach(unmount)
+
 it('shows the home page', () => {
-    const el = document.createElement('div')
-    render(<App/>, el)
     const home: Element = el.getElementsByClassName('home')[0]
+    expect(home.tagName).toEqual('MAIN')
     expect(home.innerHTML).toMatch('<h1>NACCC 2020 BOSTON</h1><h2>LABOR DAY WEEKEND 2020</h2>')
+})
+
+it('shows the nav options', () => {
     expect(el.getElementsByTagName('nav')).toHaveLength(1)
-    expect(el.getElementsByTagName('a')).toHaveLength(5)
-    unmountComponentAtNode(el)
+    expect(el.getElementsByTagName('a')).toHaveLength(4)
 })
