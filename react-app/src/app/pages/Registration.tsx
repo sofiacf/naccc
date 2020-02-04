c84d1d552c53539e1e25d43a0f12714d841ff241
Sofia <dev@smcf.io>
2020-02-03 21:48:01 -0500

    Fixes paypal button style

--- react-app/src/app/pages/Registration.tsx
+++ react-app/src/app/pages/Registration.tsx
@@ -95,7 +95,7 @@ export const Registration: React.FC = () => {
                 <fieldset>
                     <input type='hidden' name='cmd' value='_s-xclick'/>
                     <input type='hidden' name='hosted_button_id' value='EHDUC2P3MS96E'/>
-                    <input type='image' src='https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif' name='submit' alt='PayPal - The safer, easier way to pay online!'/>
+                    <input type='image' className='paypal-button' src='https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif' name='submit' alt='PayPal - The safer, easier way to pay online!'/>
                     <img alt='' className='paypal-button' src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif' width='1' height='1'/>
                 </fieldset>
             </form> }
